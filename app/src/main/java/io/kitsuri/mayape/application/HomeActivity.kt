f601c6cc597fb358dd4e0188ddff88bb4d80d8d6
Lodingglue <ayushkumar55888@gmail.com>
2025-08-08 01:39:36 +0530

    Update fix launcher

--- app/src/main/java/io/kitsuri/mayape/application/HomeActivity.kt
+++ app/src/main/java/io/kitsuri/mayape/application/HomeActivity.kt
@@ -1,18 +1,23 @@
 package io.kitsuri.mayape.application
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Button
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import io.kitsuri.mayape.manager.LauncherManager
 import io.kitsuri.mayape.ui.components.home.HomeScreen
 import io.kitsuri.mayape.ui.theme.MayaTheme
 
 class HomeActivity : ComponentActivity() {
+    private val launcherManager = LauncherManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,7 +32,19 @@ class HomeActivity : ComponentActivity() {
         setContent {
             MayaTheme {
                 HomeScreen()
+               // Button(onClick = {launchMinecraft()}) { }
             }
+        }
+
+    }
+    fun launchMinecraft() {
+        launcherManager.prepareLauncher { apks ->
+            startActivity(Intent().apply {
+                setClassName(this@HomeActivity, "com.mojang.minecraftpe.Launcher")
+                putStringArrayListExtra("APKS", ArrayList(apks))
+                flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
+            })
+            finish()
         }
     }
 }
