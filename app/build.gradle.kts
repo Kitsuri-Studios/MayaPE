49a1b7f7418821e620b19d56f15f3d4f0e4ced67
Lodingglue <ayushkumar55888@gmail.com>
2025-08-07 23:55:23 +0530

    Added NavHost Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.7")
    implementation(project(":MSA-Rapid-Fetch:MSA-Rapid-Fetch"))
     implementation(project(":TablerIcons"))
     implementation(libs.androidx.core.ktx)
