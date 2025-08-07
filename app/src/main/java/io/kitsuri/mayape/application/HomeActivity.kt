1ddc9c3a3f2527df9013db526d3ad8d117c6587e
Lodingglue <ayushkumar55888@gmail.com>
2025-08-07 23:58:55 +0530

    ft. Added Home Activity

---
+++ app/src/main/java/io/kitsuri/mayape/application/HomeActivity.kt
@@ -0,0 +1,49 @@
+package io.kitsuri.mayape.application
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import io.kitsuri.mayape.ui.components.home.HomeScreen
+import io.kitsuri.mayape.ui.theme.MayaTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        enableEdgeToEdge()
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        setContent {
+            MayaTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
